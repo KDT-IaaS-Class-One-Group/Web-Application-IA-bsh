007bbf23223f20c2dfde18c0d8da1d40456b557e
승희 <bangseunghee1108@gmail.com>
2023-11-03 17:23:15 +0900

    issue2-server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("test");
+const http = require('http');
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`)
+});
