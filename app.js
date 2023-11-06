18b986fcea732d99fffb8c2eb381f09eb2837a2e
승희 <bangseunghee1108@gmail.com>
2023-11-06 15:10:15 +0900

    서버 라우팅 시도중

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const http = require('http');
-const fs = require('fs');
-
 const express = require('express');
 const app = express();
-
 const port = 3000;
 
-app.use(express.static('public'));
-
-app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/public/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+})
+app.get('/sub', function(req, res){
+  res.sendFile(__dirname + "/public/index.html")
 })
 
-app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`)
+app.use(express.static('public'))
+
+app.use(express.json()); // JSON 파싱 미들웨어 사용
+
+app.post('/send-message', (req, res) => {
+  const message = req.body.message; // 클라이언트에서 전송된 메시지
+  // 메시지 처리 로직 추가
+  // 응답 생성
+  res.json({ status: 'success', message: `서버에서 받은 메시지: ${message}` });
 });
 
+
+app.listen(3000, function() {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+})
--- public/index.js
+++ public/index.js
@@ -1,18 +1,54 @@
-function submitButtont() {
-  const submit = document.getElementById('text').value;
+document.addEventListener('DOMContentLoaded', () => {
+  const textInput = document.getElementById('text');
+  const submitButton = document.getElementById('submit');
+  const chatContainer = document.getElementById('chatContainer');
 
-const url = "http://localhost:3000";
+  submitButton.addEventListener('click', () => {
+    const text = textInput.value;
 
-  fetch(url,{
-    method : "GET",
-    headers : {
-      'Content=Type':'application/json'
-    }
-  })
-  .then((response)=> response.json())
-  .then((data) => 
-    console.log(data))
+    // 입력된 텍스트를 채팅 컨테이너에 추가
+    const messageDiv = document.createElement('div');
+    messageDiv.textContent = text;
+    chatContainer.appendChild(messageDiv);
+
+    // 서버로 메시지 전송 (POST 요청)
+    fetch('/post-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: text }),
+    })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Data sent successfully');
+      } else {
+        console.error('Data sending failed');
+      }
+    })
     .catch((error) => {
-    console.error("API 호출 중 오류 발생", error);
+      console.error('Error:', error);
+    });
   });
-}
+});
+
+document.getElementById('messageForm').addEventListener('submit', (event) => {
+  event.preventDefault(); // 폼 제출 기본 동작을 막음
+
+  const messageInput = document.getElementById('messageInput').value;
+
+  // 서버로 메시지를 전송하는 코드를 여기에 추가
+});
+// 서버로 POST 요청 보내기 (예시)
+fetch('/send-message', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ message: messageInput })
+})
+  .then(response => response.json())
+  .then(data => {
+    // 서버 응답 처리 (예시)
+    console.log('서버 응답:', data);
+  });
