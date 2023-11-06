e684426532b1f6085f03a90804e25cd3a9fc9def
승희 <bangseunghee1108@gmail.com>
2023-11-06 16:36:48 +0900

    채팅창에 입력면 안뜸. 서버문제 json 문제

--- app.js
+++ app.js
@@ -2,25 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/public/index.html")
-})
-app.get('/sub', function(req, res){
-  res.sendFile(__dirname + "/public/index.html")
-})
+const fs = require('fs');
+app.use(express.json());
 
-app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-app.use(express.json()); // JSON 파싱 미들웨어 사용
+// JSON 파일 읽기
+const rawData = fs.readFileSync('data/data.json');
+const data = JSON.parse(rawData);
 
+// 클라이언트에서 메시지를 받아 JSON 파일에 추가
 app.post('/send-message', (req, res) => {
-  const message = req.body.message; // 클라이언트에서 전송된 메시지
-  // 메시지 처리 로직 추가
-  // 응답 생성
+  const message = req.body.message;
+  data.messages.push(message);
+
+  // JSON 파일 쓰기
+  fs.writeFileSync('data/data.json', JSON.stringify(data, null, 2));
+
   res.json({ status: 'success', message: `서버에서 받은 메시지: ${message}` });
 });
 
-
-app.listen(3000, function() {
+app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
-})
+});
--- public/index.js
+++ public/index.js
@@ -5,12 +5,52 @@ let isLeftBoxVisible = true;
 
 boxButton.addEventListener("click", () => {
   if (isLeftBoxVisible) {
-    leftBox.style.transform = "translateX(-50%)"; // leftBox를 왼쪽으로 이동하여 숨김
-    rightBox.style.transform = "translateX(-5%)"; // rightBox를 왼쪽으로 이동
+    leftBox.style.transform = "translateX(-50%)";
+    rightBox.style.transform = "translateX(-5%)";
   } else {
-    leftBox.style.transform = "translateX(0)"; // leftBox를 오른쪽으로 이동하여 보임
-    rightBox.style.transform = "translateX(0)"; // rightBox를 원래 위치로 이동
+    leftBox.style.transform = "translateX(0)";
+    rightBox.style.transform = "translateX(0)";
   }
 
-  isLeftBoxVisible = !isLeftBoxVisible; // leftBox의 표시 여부를 토글
+  isLeftBoxVisible = !isLeftBoxVisible;
 });
+
+const chat = document.getElementById('chat');
+const messageInput = document.getElementById('text');
+const messageForm = document.getElementById('messageForm');
+
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const message = messageInput.value;
+  if (message) {
+    addMessageToChat(message);
+    saveMessageToLocalStorage(message);
+    messageInput.value = '';
+  }
+});
+
+function addMessageToChat(message) {
+  const messageElement = document.createElement('p');
+  messageElement.textContent = message;
+  chat.appendChild(messageElement);
+  chat.scrollTop = chat.scrollHeight;
+}
+
+function saveMessageToLocalStorage(message) {
+  let savedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+
+  savedMessages.push(message);
+
+  localStorage.setItem('chatMessages', JSON.stringify(savedMessages));
+}
+
+function loadMessagesFromLocalStorage() {
+  const savedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+
+  savedMessages.forEach((message) => {
+    addMessageToChat(message);
+  }
+  )};
+
+loadMessagesFromLocalStorage();
