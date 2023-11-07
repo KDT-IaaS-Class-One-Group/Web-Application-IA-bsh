b0eb9a33931005e98cb3a87acc24821104a716bb
승희 <bangseunghee1108@gmail.com>
2023-11-07 12:40:29 +0900

    코드 수정

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const bodyParser = require('body-parser');
 const fs = require('fs');
 
 // 데이터 파일 경로
