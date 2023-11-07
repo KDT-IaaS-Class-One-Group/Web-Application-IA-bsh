a0603320eeb86d6cee6734419ad1c9ad5c438c63
승희 <bangseunghee1108@gmail.com>
2023-11-07 15:45:54 +0900

    작동안하는 코드 삭제

--- public/index.js
+++ public/index.js
@@ -29,7 +29,7 @@ chatBox.addEventListener('click', () => {
   chat.innerHTML = ''; 
   console.log('채팅이 초기화되었습니다.');
 });
- 
+
 // 메세지폼(form)이 제출(submit)될 때의 이벤트 핸들러를 정의
 messageForm.addEventListener('submit', (event) => {
   // 기본 동작을 취소하는 역할 (폼 제출시 이벤트를 다시 로드하는 동작을 막음)
@@ -40,13 +40,10 @@ messageForm.addEventListener('submit', (event) => {
     // 채팅창에 추가
     addMessage(message);
     // 로컬 스토리지(Local Storage)에 저장
-    saveMessage(message);
-    // 메시지를 전송한 후에 다음 메시지를 입력할 수 있도록 비움
-    datatext.value = '';
   }
 });
 
-
+// 채팅창에 글을 작성하면 채팅장에 입력 됨
 function addMessage(message) {
   // 'p'요소 동적으로 생성
   const messageElement = document.createElement('p');
@@ -54,15 +51,6 @@ function addMessage(message) {
   chat.appendChild(messageElement);
   // 채팅창을 맨 아래로 스크롤하는 역할
   chat.scrollTop = chat.scrollHeight;
-}
-
-// 함수를 사용하여 메시지를 로컬 스토리지(Local Storage)에 저장
-function saveMessage(message) {
-  // Local Storage에 저장된 값을 가져오는 작업
-  let savedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
-  savedMessages.push(message);
-  // 새로운 메시지를 추가하고 로컬 스토리지에서 이전에 저장된 메시지 목록을 갱신
-  localStorage.setItem('chatMessages', JSON.stringify(savedMessages));
 }
 
 // "sendbutton" 클릭 이벤트
